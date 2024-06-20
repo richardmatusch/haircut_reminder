2ebce9d3eea966659ef267a631a708762d136587
richardmatusch <richardmatusch23@gmail.com>
2024-06-20 19:33:01 +0200

    added icon

--- reminder.py
+++ reminder.py
@@ -1,6 +1,11 @@
 from win11toast import toast
 from datetime import datetime, timedelta
 import ephem
+import os
+
+# getting the script directory and setting up the icon for the notification. code assumes that the icon is in the same folder as python file
+script_dir = os.path.dirname(__file__)
+icon_path = os.path.join(script_dir, "lion.png") # <a href="https://www.flaticon.com/free-icons/lion" title="lion icons">Lion icons created by Freepik - Flaticon</a>
 
 def find_match(date_time, operator, time_delta):
     """find and return closest date -1 timedelta where the moon is in waxing phase, and in leo constellation. you can go backwards or forwards from entered datetime value by selecting operator and increase precision with timedelta increment setting"""
@@ -29,21 +34,20 @@ def find_match(date_time, operator, time_delta):
 
 def message():
     """message for the toast notification"""
-    if time_till_window_start < timedelta(days=20) and window_start > current_time:
+    if time_till_window_start < timedelta(days=20) and window_start > current_date_time:
         return (f"Najbližší rastúci mesiac v levovi začne:\n{window_start.strftime('%d/%m/%Y o %H:%M')}")
-    elif current_time >= window_start and current_time <= window_end:
+    elif current_date_time >= window_start and current_date_time <= window_end:
         return (f"Mesiac rastie a je v levovi do:\n{window_end.strftime('%d/%m/%Y, %H:%M')}")
 
 # calculations of needed values for notification
-current_time = datetime.now()
-window_start = find_match(find_match(find_match(current_time, "+", "days"), "+", "hours"), "+", "minutes")
+current_date_time = datetime.now()
+window_start = find_match(find_match(find_match(current_date_time, "+", "days"), "+", "hours"), "+", "minutes")
 window_end = find_match(find_match(find_match(window_start + timedelta(days=5), "-", "days"), "-", "hours"), "-", "minutes")
-# remaining_window = window_end - current_time
-time_till_window_start = window_start - current_time
+time_till_window_start = window_start - current_date_time
 
 message = message()
 
 if message:
-    toast(message, duration="long")
+    toast(message, icon=icon_path, scenario="incomingCall")
 
 
