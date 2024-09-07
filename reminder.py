0adb35e1c8d9b655d092c3084545ff811aa3d9c7
richardmatusch <richardmatusch23@gmail.com>
2024-09-07 21:01:12 +0200

    added function for correct calculation of waxing phase

--- reminder.py
+++ reminder.py
@@ -2,9 +2,22 @@ from win11toast import toast
 from datetime import datetime, timedelta
 import ephem
 
+m = ephem.Moon()
+
+def is_waxing(date_time):
+    """determine if the moon is in waxing phase"""
+    m.compute(date_time)
+    moon_phase_now = m.phase
+
+    m.compute(date_time + timedelta(hours=1))
+    moon_phase_plus_one_hour = m.phase
+    
+    if moon_phase_plus_one_hour > moon_phase_now:
+        return True
+
+
 def find_match(date_time, operator, time_delta):
     """find and return closest date -1 timedelta where the moon is in waxing phase, and in leo constellation. you can go backwards or forwards from entered datetime value by selecting operator and increase precision with timedelta increment setting"""
-    m = ephem.Moon()
     
     time_deltas = {
         'minutes': timedelta(minutes=1),
@@ -19,7 +32,6 @@ def find_match(date_time, operator, time_delta):
         m.compute(date_time)
         constellation = ephem.constellation(m)
         moon_phase = m.phase
-        # here is a mistake... moonphase condition is in correct percentage but it is not reflecting on whether the moon is in waxing or waning phase...
         if constellation[1] == "Leo" and (moon_phase > 0 and moon_phase < 100):
             found_match = date_time - delta # i am subtracting delta because i don't want to overshoot start of the window
         if operator == "+":
@@ -28,15 +40,17 @@ def find_match(date_time, operator, time_delta):
             date_time -= delta
     return found_match
 
+
 def message():
     """message for the toast notification"""
-    if time_till_window_start < timedelta(days=20) and window_start > current_date_time:
+    if time_till_window_start < timedelta(days=2) and window_start > current_date_time and is_waxing:
         return (f"Najbližší rastúci mesiac v levovi začne:\n{window_start.strftime('%d/%m/%Y o %H:%M')}")
     elif current_date_time >= window_start and current_date_time <= window_end:
         return (f"Mesiac rastie a je v levovi do:\n{window_end.strftime('%d/%m/%Y, %H:%M')}")
 
 # calculations of needed values for notification
-current_date_time = datetime.now()
+current_date_time = datetime.now() 
+is_waxing = is_waxing(current_date_time)
 window_start = find_match(find_match(find_match(current_date_time - timedelta(days=5), "+", "days"), "+", "hours"), "+", "minutes")
 window_end = find_match(find_match(find_match(window_start + timedelta(days=5), "-", "days"), "-", "hours"), "-", "minutes")
 time_till_window_start = window_start - current_date_time
